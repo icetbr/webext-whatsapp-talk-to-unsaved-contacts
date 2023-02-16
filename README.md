4d4a5dfaa81bfb5ca157a98c7274b2e2c998f966
icetbr <icetbr@yahoo.com.br>
2023-02-16 13:48:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# WhatsApp Talk to Unsaved Contacts
+You don't have to leave the WhatsWeb interface to talk to unsaved contacts anymore!
+
+### After
+![After](media/after.jpg)
+
+
+## Install
+[Userscript][1] - [Firefox][2] - [Chrome][3].
+
+
+## Usage
+Press the phone button, input the phone number and hit ENTER. This will reload the current page, opening a chat window to the unsaved phone number.
+
+
+## Why
+<!-- Focus on explaining what the item does and why users should install it -->
+I use mainly WhatsApp on my notebook. This allows me to quickly message many services to requests budgets, or to check wether they provide what I need.
+
+It does the same thing online link generators like [create.wa](https://create.wa.link/) do, but without leaving the WhatsApp web interface. It calls WhatsApp API to contact an unlisted phone number.
+
+
+[Contributing](https://github.com/icetbr/my-projects/blob/main/CONTRIBUTING.md)\
+[License (MIT)](https://choosealicense.com/licenses/mit/)
+
+[1]: https://openuserjs.org/scripts/icetbr/WhatsApp_Talk_to_Unsaved_Contacts
+[2]: https://addons.mozilla.org/en-US/firefox/addon/whatsapp-talk-unsaved-contacts
+[3]: https://chrome.google.com/webstore/detail/webext-whatsapp-talk-to-unsaved-contacts/iaccemlleljjljglecinjhogonmhnbac
---
+++ src/content.js
@@ -0,0 +1,43 @@
+import { $ } from '@icetbr/utils/web';
+import { waitForEl, el2 } from '@icetbr/utils/web';
+
+var init = () => {
+    const redirect = e => {
+        if (e.key !== 'Enter') return;
+        //5547997305822
+        window.location.href=`https://web.whatsapp.com/send/?phone=${e.currentTarget.value}&text&type=phone_number&app_absent=0`;
+    }
+    window.icetbr_redirect = redirect;
+
+    const style = 'outline: none; border:none; background-image:none; background-color:transparent; -webkit-box-shadow: none; -moz-box-shadow: none; box-shadow: none; display: none; font-size: 14px; top: -2px;';
+    const $input = el2(`<input id="unsavedContactPhoneNumber" placeholder="Phone number" title: "Ex: 5547999783456" class="Er7QU" style="${style}" onkeyup="icetbr_redirect(event)">`);
+
+    const $searchBox = $('._3RN1i._3jaYl');
+    $searchBox.appendChild($input);
+
+
+    const hidden = true;
+    const togglePhoneNumber = () => {
+        $('#unsavedContactPhoneNumber').style.display = hidden ? 'block' : 'none';
+        hidden = !hidden;
+    }
+    window.icetbr_togglePhoneNumber = togglePhoneNumber;
+
+    const iconSvg = `
+        <svg viewBox="0 0 28 21" height="24" width="17" preserveAspectRatio="xMidYMid meet" class="" fill="none">
+        <path fill="currentColor" d="M7.043 9.306c-3.85 2.223 2.024 13.234 6.237 10.802l3.906 6.765c-1.777 1.026-3.282 1.673-5.342.495C6.122 24.098-.176 13.194.004 6.69.066 4.431 1.397 3.546 3.138 2.541l3.906 6.765zm1.615-.184c-.416.24-.951.097-1.191-.319L3.784 2.425c-.24-.416-.097-.951.319-1.191L6.039.116c.416-.24.951-.097 1.192.319l3.682 6.378c.24.416.097.951-.319 1.191L8.658 9.122zm10.059 17.423c-.416.24-.951.097-1.191-.319l-3.682-6.378c-.24-.416-.097-.951.319-1.191l1.935-1.117c.416-.24.951-.097 1.192.319l3.682 6.378c.24.416.097.951-.319 1.191l-1.935 1.117z"/>
+        </svg>
+    `
+    const $button = el2(`
+        <div class="_3OtEr rOo0o" style="left: 15px; top: -1px;"><div class="_3ndVb" role="button" title="Send message to unsaved contact" onclick="icetbr_togglePhoneNumber()"><span>
+        ${iconSvg}
+        </span></div><span></span></div>
+    `)
+
+    // var $communityButton = $('[data-testid="menu-bar-community-tab"]');
+    const $buttonsBar = $('._1sPvB._2XdMx span');
+    $buttonsBar.prepend($button);
+
+}
+
+waitForEl('[data-testid="menu-bar-community-tab"]').then(init);
